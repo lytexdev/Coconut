db19d1efecd05ac78517d7d863f1fa8c25ff0a46
Immanuel Mruk <immanuelmruk@gmail.com>
2024-06-15 02:39:26 +0200

    Add confirmation to reboot or shutdown

--- coconut-shell/src/components/modules/SystemInfo.vue
+++ coconut-shell/src/components/modules/SystemInfo.vue
@@ -7,46 +7,74 @@
             <p>Disk Usage: {{ diskUsage }}</p>
         </div>
         <div class="right-panel">
-            <button @click="shutdown" class="btn btn-shutdown" title="Poweroff computer">Shutdown</button>
-            <button @click="reboot" class="btn btn-reboot" title="Reboot computer">Reboot</button>
+            <button @click="showModal('shutdown')" class="btn btn-shutdown" title="Poweroff computer">Shutdown</button>
+            <button @click="showModal('reboot')" class="btn btn-reboot" title="Reboot computer">Reboot</button>
         </div>
+
+        <Modal 
+            v-if="modalVisible" 
+            :title="modalTitle" 
+            :visible="modalVisible" 
+            :confirmText="modalConfirmText" 
+            :cancelText="'Cancel'" 
+            @close="modalVisible = false" 
+            @confirm="performAction">
+            <p>Are you sure you want to {{ modalAction }} the system?</p>
+        </Modal>
     </div>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue'
+import Modal from './Modal.vue'
 
 const cpuUsage = ref('Loading...')
 const ramUsage = ref('Loading...')
 const diskUsage = ref('Loading...')
+const modalVisible = ref(false)
+const modalTitle = ref('')
+const modalConfirmText = ref('')
+const modalAction = ref('')
 
 const updateSystemInfo = () => {
-    fetch('/api/system_info')
-        .then((response) => response.json())
-        .then((data) => {
-            cpuUsage.value = data.cpu_usage
-            ramUsage.value = data.ram_usage
-            diskUsage.value = data.disk_used
-        })
-        .catch((error) => console.error('Error fetching system info:', error))
+  fetch('/api/system_info')
+    .then((response) => response.json())
+    .then((data) => {
+      cpuUsage.value = data.cpu_usage
+      ramUsage.value = data.ram_usage
+      diskUsage.value = data.disk_used
+    })
+    .catch((error) => console.error('Error fetching system info:', error))
 }
 
-const shutdown = () => {
-    fetch('/api/shutdown', { method: 'POST' })
-        .then((response) => response.json())
-        .then((data) => console.log('Shutdown initiated'))
-        .catch((error) => console.error('Error shutting down:', error))
+const showModal = (action) => {
+  modalAction.value = action
+  modalTitle.value = action.charAt(0).toUpperCase() + action.slice(1)
+  modalConfirmText.value = action.charAt(0).toUpperCase() + action.slice(1)
+  modalVisible.value = true
 }
 
-const reboot = () => {
-    fetch('/api/reboot', { method: 'POST' })
-        .then((response) => response.json())
-        .then((data) => console.log('Reboot initiated'))
-        .catch((error) => console.error('Error rebooting:', error))
+const performAction = () => {
+  const action = modalAction.value
+  const urls = {
+    shutdown: '/api/shutdown',
+    reboot: '/api/reboot'
+  }
+  
+  fetch(urls[action], { method: 'POST' })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(`${action.charAt(0).toUpperCase() + action.slice(1)} initiated`)
+      modalVisible.value = false
+    })
+    .catch((error) => {
+      console.error(`Error ${action}ing:`, error)
+      modalVisible.value = false
+    })
 }
 
 onMounted(() => {
-    updateSystemInfo()
-    setInterval(updateSystemInfo, 2000)
+  updateSystemInfo()
+  setInterval(updateSystemInfo, 2000)
 })
 </script>
