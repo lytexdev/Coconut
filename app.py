4a7e119a4c7aae30ea9506fe64c40b153c04a707
Immanuel Mruk <immanuel.mruk@8mylez.com>
2024-07-17 11:41:58 +0200

    Fix docker issue

--- app.py
+++ app.py
@@ -5,36 +5,53 @@ from config import Config
 from flask_migrate import Migrate
 from models import db, User, Setup
 
-app = Flask(__name__, static_folder="static", static_url_path="", template_folder="templates")
-app.config.from_object(Config)
-rate_limiter = Limiter(get_remote_address, app=app, default_limits=[Config.RATE_LIMIT])
-
-db.init_app(app)
-migrate = Migrate(app, db)
-
 from views.auth import auth_bp
 from views.main import main_bp
 from views.setup import setup_bp
 from api.system_info import system_info_bp
 from api.backup import backup_bp
-from api.docker_container import docker_bp
 
+
+# ----------------- Flask App ----------------- #
+app = Flask(__name__, static_folder="static", static_url_path="", template_folder="templates")
+app.config.from_object(Config)
+rate_limiter = Limiter(get_remote_address, app=app, default_limits=[Config.RATE_LIMIT])
+
+
+# ----------------- Database ----------------- #
+db.init_app(app)
+migrate = Migrate(app, db)
+
+
+# ----------------- Blueprints ----------------- #
 app.register_blueprint(auth_bp)
 app.register_blueprint(main_bp)
 app.register_blueprint(setup_bp, url_prefix="/setup")
 app.register_blueprint(system_info_bp, url_prefix="/api")
 app.register_blueprint(backup_bp, url_prefix="/api")
-app.register_blueprint(docker_bp, url_prefix="/api")
 
 
+# ----------------- Docker Integration ----------------- #
+try:
+    import docker
+    client = docker.from_env()
+    from api.docker_container import docker_bp
+    app.register_blueprint(docker_bp, url_prefix="/api")
+except (ImportError, docker.errors.DockerException) as e:
+    print("Docker is not available. Skipping Docker integration.")
+
+
+# ----------------- Setup Check ----------------- #
 @app.before_request
 def check_for_setup():
     setup_record = Setup.query.first()
     if not User.query.first() or (setup_record and not setup_record.completed):
-        if request.endpoint not in ["setup.create_user", "setup.set_modules", "setup.get_setup_status", "setup.finish_setup", "setup.setup_index"]:
+        if request.endpoint not in ["setup.create_user", "setup.set_modules", "setup.finish_setup", "setup.setup_index"]:
             if request.endpoint and not request.endpoint.startswith("static"):
                 return redirect(url_for("setup.setup_index"))
 
+
+# ----------------- Index Route ----------------- #
 @app.route("/")
 def index():
     return render_template("index.html")
