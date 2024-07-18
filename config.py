dbc0cd6353e9f40abee821e0ee005de83a978ef5
Immanuel Mruk <immanuelmruk@gmail.com>
2024-07-19 00:20:47 +0200

    Fix too many requests issue

--- config.py
+++ config.py
@@ -5,11 +5,9 @@ import bcrypt
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY')
+    SECRET_KEY = os.getenv('SECRET_KEY', 'supersecretkey')
     SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     PORT = os.getenv('PORT', 8080)
     DEBUG = os.getenv('DEBUG', False)
-    RATE_LIMIT = os.getenv('RATE_LIMIT', '5 per minute')
-    SHUTDOWN_COMMAND = os.getenv('SHUTDOWN_COMMAND', 'sudo shutdown -h now')
-    REBOOT_COMMAND = os.getenv('REBOOT_COMMAND', 'sudo reboot')
+    RATE_LIMIT = os.getenv('RATE_LIMIT', '60 per minute')
